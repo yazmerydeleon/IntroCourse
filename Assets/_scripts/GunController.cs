de2e3556f3115720a5a5f92ce5a0e32545f05ee2
Holiday Course <holiday@ad.aie.edu>
2018-06-20 20:58:40 -0700

    added gun script

---
+++ Assets/_scripts/GunController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour {
+
+    public int Mag = 20;
+
+    public float FireRate = 0.1f;
+
+    private float SetFiredDelay = 0;
+
+    public GameObject BulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void Fire()
+    {
+        
+            if (Mag > 0 && SetFiredDelay <= 0)
+            {
+            //Debug.Log("Fire!!");
+            GameObject TempBullet =  Instantiate( BulletPrefab, transform.position, transform.rotation);
+            TempBullet.GetComponent<Rigidbody>().velocity = transform.forward * 100;
+
+                SetFiredDelay = FireRate;
+                Mag --;
+                Debug.Log(Mag);
+            }           
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        SetFiredDelay -= Time.deltaTime;
+		
+	}
+}
--- Assets/_scripts/PlayerController.cs
+++ Assets/_scripts/PlayerController.cs
@@ -4,27 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+    GunController playerGun;
+
     private Rigidbody RB;
+    private Vector3 IP;
+    private Camera cam;
 
     public float SprintAccelerationSpeed;
     public float WalkAccelerationSpeed;
-
     public float MaxWalkSpeed;
     public float MaxSprintSpeed;
-
+    public float RotateSpeed;
 
     private float SetMaxSpeed;
     private float SetAccelerationSpeed;
 
     public Animator Anim;
 
-    private Vector3 IP;
+    public GunController gun;
 
     float test;
+
 	// Use this for initialization
 	void Start () {
 
         RB = GetComponent<Rigidbody>();
+        cam = GameObject.FindObjectOfType<Camera>();
+        if(cam == null)
+        {
+            Debug.Log("NO CAMERA");
+        }
 	}
 
     public void updateAnim()
@@ -40,8 +49,14 @@ public class PlayerController : MonoBehaviour {
         IP.x = Input.GetAxisRaw("Horizontal");
         IP.z = Input.GetAxisRaw("Vertical");
 
+        if (Input.GetMouseButton(0))
+        {
+            gun.Fire();
+        }
+
         SetMaxSpeed = (Input.GetButton("Sprint")) ? MaxSprintSpeed : MaxWalkSpeed;
         SetAccelerationSpeed = (Input.GetButton("Sprint")) ? SprintAccelerationSpeed : WalkAccelerationSpeed;
+
     }
 
     public void handleMovement()
@@ -55,12 +70,30 @@ public class PlayerController : MonoBehaviour {
 
     }
 
-	// Update is called once per frame
-	void Update () {
+    public void doMouseLook()
+    {
+        RaycastHit hit;
+
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(ray, out hit, 10000))
+        {
+            Vector3 forward = (transform.position - hit.point).normalized * -1;
+
+            transform.forward = Vector3.MoveTowards(transform.forward, forward, RotateSpeed * Time.deltaTime);
+            transform.eulerAngles = new Vector3(0,transform.eulerAngles.y,0);
+            //Debug.Log(hit.point);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
 
         keyInput();
         handleMovement();
         updateAnim();
+        doMouseLook();
        
         //Debug.Log(test);
 	}
